045af3a796ac58cc899163614aad89112e72350c
megan-kd <deyo2303@pacificu.edu>
2019-11-13 00:14:34 -0800

    added item and image inserts (julian) added people, professor, printer, and image inserts

--- BigDBInserts.sql
+++ BigDBInserts.sql
@@ -76,55 +76,191 @@ INSERT INTO AdjacentBuildings (BuildingID, AdjacentBuildingID) VALUES (7, 6); --
 INSERT INTO AdjacentBuildings (BuildingID, AdjacentBuildingID) VALUES (7, 4); --repeat
 INSERT INTO AdjacentBuildings (BuildingID, AdjacentBuildingID) VALUES (7, 3); --repeat
 
+--image inserts
+-- item icons
+INSERT INTO Images (Image, Type) -- 1
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Doritos.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 2
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Pokeball.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 3
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Stapler.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 4
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/SSD.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 5
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Lightsaber.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 6
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Charger.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 7
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Marker.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 8
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Semicolon.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 9
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/WICSTee.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 10
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/Corn.png'), "mime/png");
+
+INSERT INTO Images (Image, Type) -- 11
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/WateringCan'), "mime/png");
+
+-- cs people icons
+--chadd
+INSERT INTO Images (Image, type) -- 12
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/chadd2.png'), "mime/png");
+--cooper
+INSERT INTO Images (Image, type) -- 13
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/cooper.png', "mime/png");
+--larry
+INSERT INTO Images (Image, type) -- 14
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/larry.png'), "mime/png");
+--sam
+INSERT INTO Images (Image, type)-- 15
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/sam.png'), "mime/png");
+--doug
+INSERT INTO Images (Image, type)-- 16
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/doug.png'), "mime/png");
+--megan
+INSERT INTO Images (Image, type)-- 17
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/megan.png'), "mime/png");
+--shereen
+INSERT INTO Images (Image, type) -- 18
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/shereen.png'), "mime/png");
+--chris lane
+INSERT INTO Images (Image, type) -- 19
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/chrislane.png'), "mime/png");
+--julian
+INSERT INTO Images (Image, type) -- 20
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/julian.png'), "mime/png");
+--mystery student
+INSERT INTO Images (Image, type) -- 21
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/mysterystudent.png'), "mime/png");
+--thomas
+INSERT INTO Images (Image, type) -- 22
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/thomas.png'), "mime/png");
+--sarah
+INSERT INTO Images (Image, type) -- 23
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/sarah.png'), "mime/png");
+--audrey
+INSERT INTO Images (Image, type) -- 24
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/audrey.png'), "mime/png");
+-- chugg
+INSERT INTO Images (Image, type) -- 25
+VALUES (LOAD_FILE('/tmp/g5/Pictures/csdeptpeoplepics/chuggforgame.png'), "mime/png");
+
+-- printer photo
+INSERT INTO Images (Image, type) -- 26
+VALUES (LOAD_FILE('/tmp/g5/Pictures/g5DBPhotos/printer.PNG'), "mime/png");
+
+
+--item inserts
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Doritos", 1, 1, 1);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Pokeball", 1, 2, 2);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Stapler", 1, 3, 3);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("SSD", 1, 4, 4);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Lightsaber", 1, 5, 5);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Phone Charger", 1, 6, 6);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Whiteboard Marker", 1, 7, 7);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Semicolon Keycap", 1, 8, 8);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("WICS Tee", 1, 9, 9);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Ear of Corn", 1, 10, 10);
+
+INSERT INTO Items (Name, Weight, ImageID, ItemHintID)
+VALUES ("Watering Can", 1, 11, 11);
+
+
 -- printer (just one for now)
--- INSERT INTO Printers (Name, ImageID, RoomID) VALUES ("The TIC",  ,5);
+INSERT INTO Printers (Name, ImageID, RoomID) VALUES ("The TIC", 26 ,5);
 
 --we inserting npc's (people, professors, students)
 -- people
 -- shereen
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Shereen, Khoja); --pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID) VALUES (Shereen, Khoja, 13, 7, 1, 18);
 
 -- chadd
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Chadd, Williams); --pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Chadd, Williams, 12, 5, 1, 12); 
 
 -- doug
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Doug, Ryan); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Doug, Ryan, 14, 10, 1, 16); 
 
 -- chris lane
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Chris, Lane); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Chris, Lane, 15, 9, 1, 19); 
 
 -- audrey 
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Audrey, Lincoln); -- pic submitted
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Audrey, Lincoln, 20, 11, 1, 24); 
 
 -- cooper
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Cooper, Delay); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Cooper, Delay, 23, 1, 1, 13); 
 
 --sam
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Sam, Sindt); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Sam, Sindt, 21, 8, 1, 15); 
 
 -- julian
---INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Julian, Bunch);
+INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+VALUES (Julian, Bunch, 19, 6, 1, 20);
 
 -- thomas
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Thomas, Robasciotti); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID) 
+ VALUES (Thomas, Robasciotti, 18, 3, 1, 22); 
 
 -- mystery student
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Mystery, Student); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID) 
+ VALUES (Mystery, Student, 24, 4, 1, 21); 
 
 -- megan
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Megan, DeYoung);
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Megan, DeYoung, 17, 4, 1, 17); 
 
 -- chougg
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Chugg, Ryams); -- pic done
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Chugg, Ryams, 25, 1, 1, 25); 
 
 -- larry
--- INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID) VALUES (Larry, Jensen);
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Larry, Jensen, 22, 2, 1, 14);
 
 --sarah
--- INSERT INTO People (FName, LName, SoundID, DialogueID, ItemID, PrinterID) VALUES (Sarah, Amano); -- pic submitted
+ INSERT INTO People (FName, LName, DialogueID, ItemID, PrinterID, PictureID)
+ VALUES (Sarah, Amano, 16, 2, 1, 23);
 
 
-
+-- Professors
+INSERT INTO Professors (ProfID) VALUES (1);
+INSERT INTO Professors (ProfID) VALUES (2);
+INSERT INTO Professors (ProfID) VALUES (3);
+INSERT INTO Professors (ProfID) VALUES (4);
 
 
 
